3ef70cba086c5d58e137efed08a74592fae28e27
Elizabeth Alfaro-Espinoza <71552610+ElizaAlfaro@users.noreply.github.com>
2022-07-23 16:24:20 -0300

    add the basics file

---
+++ python1/basico.py
@@ -0,0 +1,54 @@
+from numpy import intp
+
+'''
+# Hola mundo
+- Printando nome e idade
+'''
+#nome = input('Digite seu nome: ')
+#print('Benvinda, {}!'.format(nome))
+#idade = input('Digite sua idade: ')
+#print('Você tem {} anos.'.format(idade))
+
+'''
+# Printando na tela
+- 'É possível printar vários valores em uma única linha'
+'''
+#x = 3.0
+#y = 2
+#print('Divisão: x = {}'.format(x/y))
+
+'''
+# Pilha
+- 'O último a entrar é o primeiro a sair'
+'''
+pilha = [1, 1, 2, 3, 5]
+pilha.append(13) #último a entrar
+pilha.pop() #é o primeiro a sair
+
+'''
+# Fila
+- 'O primeiro a entrar é o primeiro a sair'
+'''
+fila = [1, 1, 2, 3, 5]
+fila.pop(0) #primeiro a sair
+
+'''
+## Tupla
+- Inmutável
+'''
+coordenadaA = (4,6)
+coordenadaB = (5,7)
+print('distancia entre A e B: {}'.format(abs(coordenadaA[0]-coordenadaB[0])+abs(coordenadaA[1]-coordenadaB[1])))
+
+'''
+# Listas
+- Listas são mutáveis
+'''
+cachorros = ['Fido', 'Rex', 'Bobby']
+print(cachorros[:2])
+print(cachorros[1:])
+print(cachorros[::-1])
+
+nome_removido = cachorros.pop(2)
+print(cachorros)
+print(nome_removido)
